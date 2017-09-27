5c0d8734c4b7b8136abd9f0f7d994f8f4c1f3f1f
RYAN CONWAY - Student <G00332826@gmit.ie>
2017-09-27 10:37:44 +0100

    NewtonsQ9

---
+++ goProblems1.go
@@ -0,0 +1,17 @@
+func z_next(z float64, x float64) float64{
+	return z - (((z * z) - x) / (2 * z))
+}
+
+func main() {
+	x := 20.0
+	
+	var z float64
+	
+	for z = 1.0; z != z_next(z, x); z = z_next(z, x) {
+		fmt.Printf("Current guess: %12.8f\n", z)
+	}
+
+	fmt.Printf("The square root of %f is %f \n", x, z)
+	
+	fmt.Printf("The math.sqrd Calculation is %f", Math.sqrd);
+}
